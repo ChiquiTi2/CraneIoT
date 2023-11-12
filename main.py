ff1a1cda160453c0941851f15fbb639e8f89c757
Tim Bischoff <tim-bischoff@gmx.de>
2023-11-12 12:26:36 +0100

    Changed code processing to run with asyncio

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from crane_simulation.sensors import Crane
-import continuous_threading
 
 
 if __name__ == '__main__':
     My_Crane = Crane(1)
-    # My_Crane.run_crane()
-    print(My_Crane.rot_sensor().render())
-    try:
-        #sensor_thread = continuous_threading.Thread(target=temp_sensor, name="Sensor_Thread")
-        #sensor_thread.start()
-        pass
-    except:
-        print("Unable to start thread")
+    My_Crane.run_crane()
