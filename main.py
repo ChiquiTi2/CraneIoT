888d0d98ac4c3008620b6e976d54dee4a9e66d8f
Tim Bischoff <tim-bischoff@gmx.de>
2023-11-18 21:02:26 +0100

    prepared NMEA server class, tryed asyncio in main function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from crane_simulation.sensors import Crane
+from crane_simulation import NmeaServer
+import asyncio
 
 
 if __name__ == '__main__':
-    My_Crane = Crane(1)
-    My_Crane.run_crane()
+    # My_Crane = Crane(1)
+    # My_Crane.run_crane()
+    # Server = NmeaServer()
+    loop = asyncio.get_event_loop()
+    server = NmeaServer('127.0.0.1',8888, loop)
+    asyncio.run(Server.initiate_async_server())
